77ccac00d1e6260dad86bdcde0d15407133f464c
TimGou <tgou1055@gmail.com>
2024-11-11 01:33:56 +1100

    spark-job:2024-11-10: Functions (Doing)

--- data-processing-spark/2-apache-spark-basics/spark-data-processing-101/functions_new.py
+++ data-processing-spark/2-apache-spark-basics/spark-data-processing-101/functions_new.py
@@ -1,3 +1,59 @@
 """
-Functions
+SQL In-Build Functions
 """
+
+from pyspark.sql import SparkSession # type: ignore
+from pyspark.sql.functions import ( # type: ignore
+    length, concat, split, substring, trim,
+    datediff, months_between, date_add, date_format, to_date, to_timestamp,
+    year, ceil, floor, round, abs,
+    col, lit
+) 
+
+def run_code(spark):
+    """
+    Spark code runner
+
+    params:
+        :param: spark: sparkSession object
+
+    returns:
+
+    """
+    print("=======================================")
+    print("Using tpch database")
+    print("=======================================")
+    spark.sql("USE tpch")
+
+    print("=======================================")
+    print("Calculating length of 'hi'")
+    print("=======================================")
+    length_df = spark.createDataFrame([('hi',)], ['string'])
+    length_df = length_df.select(length(length_df['string']).alias('length'))
+    length_df.show()
+
+    print("=======================================")
+    print("Concatenating clerk and orderpriority with '-'")
+    print("=======================================")
+    concat_df = (
+        spark.table("orders")
+        .select(
+            concat(col("clerk"), lit("-"), col("orderpriority"))
+            .alias("concatenated")
+        )
+        .limit(10)
+    )
+    concat_df.show()
+
+
+
+if __name__ == '__main__':
+    spark_obj = (
+        SparkSession.builder.appName("efficient-data-processing-spark")
+        .enableHiveSupport()
+        .getOrCreate()
+    )
+    # Set the log level
+    spark_obj.sparkContext.setLogLevel("ERROR")
+    run_code(spark=spark_obj)
+    spark_obj.stop
--- data-processing-spark/2-apache-spark-basics/spark-data-processing-101/functions_new.sql
+++ data-processing-spark/2-apache-spark-basics/spark-data-processing-101/functions_new.sql
@@ -1,3 +1,69 @@
 /*
-
+SQL in-build functions
 */
+
+-- String operations
+
+SELECT LENGTH('hi')
+
+USE tpch;
+
+SELECT 
+    CONCAT(clerk, '-', orderpriority) 
+FROM
+    orders
+LIMIT 
+    5;
+
+SELECT
+    SPLIT(clerk, '#') 
+FROM 
+    orders 
+LIMIT 
+    5;
+
+-- SUBSTR (Extracts the first 5 characters from the clerk column.)
+SELECT
+    clerk,
+    SUBSTR(clerk,1,5)
+FROM
+    orders
+LIMIT
+    5;
+
+SELECT TRIM(' hi ');
+
+-- DATE operations
+SELECT
+    DATEDIFF('2022-11-05', '2022-10-01') diff_in_days,
+    MONTHS_BETWEEN('2022-11-05', '2022-10-01') diff_in_months,
+    MONTHS_BETWEEN('2022-11-05', '2022-10-01') / 12  diff_in_years,
+    DATE_ADD('2022-11-05', 10) AS new_date;
+
+SELECT 
+    DATE '2022-11-05' + INTERVAL '10' DAY;
+
+SELECT 
+    TO_DATE('11-05-2023', 'MM-dd-yyyy') AS parse_date, 
+    TO_TIMESTAMP('11-05-2023', 'MM-dd-yyyy') AS parse_ts;
+
+SELECT 
+    DATE_FORMAT(orderdate, 'yyyy-MM-01') AS first_month_date
+FROM
+    orders
+LIMIT 5;
+-- This query will return the date as YYYY-MM-01, 
+-- effectively giving the first day of each month based on the orderdate.
+
+SELECT
+    YEAR(DATE '2024-11-10');
+
+-- NUMBER operations
+SELECT
+    ROUND(100.102345, 2);
+
+SELECT 
+    ABS(-100), ABS(100);
+
+SELECT
+    CEIL(100.1), FLOOR(100.1);
